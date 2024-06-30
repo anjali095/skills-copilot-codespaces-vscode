01294ca6e1398668b988801a583ca4e204aba4dd
Anjali Sharma <asharma7_be20@thapar.edu>
2024-06-30 18:24:39 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -3,38 +3,40 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var path = require('path');
-var port = 3000;
-
-//create server
-http.createServer(function(req, res){
-    var uri = url.parse(req.url).pathname;
-    var filename = path.join(process.cwd(), uri);
-
-    fs.exists(filename, function(exists){
-        if(!exists){
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.write('404 Not Found\n');
-            res.end();
-            return;
-        }
-
-        if(fs.statSync(filename).isDirectory()){
-            filename += '/index.html';
-        }
-
-        fs.readFile(filename, 'binary', function(err, file){
-            if(err){
-                res.writeHead(500, {'Content-Type': 'text/plain'});
-                res.write(err + '\n');
-                res.end();
-                return;
+var comments = [];
+var server = http.createServer(function (request, response) {
+    var urlObj = url.parse(request.url, true);
+    var urlPathName = urlObj.pathname;
+    if (urlPathName === '/') {
+        fs.readFile(path.join(__dirname, 'index.html'), function (err, data) {
+            if (err) {
+                response.end('404 Not Found.');
+            } else {
+                response.end(data);
             }
-
-            res.writeHead(200);
-            res.write(file, 'binary');
-            res.end();
         });
-    });
-}).listen(port);
-
-console.log('Server is running at http://localhost:' + port + '/');
+    } else if (urlPathName === '/post') {
+        fs.readFile(path.join(__dirname, 'post.html'), function (err, data) {
+            if (err) {
+                response.end('404 Not Found.');
+            } else {
+                response.end(data);
+            }
+        });
+    } else if (urlPathName === '/comment') {
+        var comment = urlObj.query;
+        comments.push(comment);
+        response.end(JSON.stringify(comments));
+    } else {
+        fs.readFile(path.join(__dirname, urlPathName), function (err, data) {
+            if (err) {
+                response.end('404 Not Found.');
+            } else {
+                response.end(data);
+            }
+        });
+    }
+});
+server.listen(3000, function () {
+    console.log('server is listening at port 3000');
+});
